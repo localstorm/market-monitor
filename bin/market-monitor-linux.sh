5d51d5cb15dcde3b716ace0d93b498c70c289b6c
localstorm <alexey@kuznetsov.co>
2014-03-26 18:18:48 +0400

    Memory footprint optimizations

--- bin/market-monitor-linux.sh
+++ bin/market-monitor-linux.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 export SPREADS=~/Dropbox/Covert/Finance/Options/SpreadsMonitoring/spreads.txt
 export CLASSPATH=../lib/*:../sounds:../lib/linux64/*:../out/production/market-monitor
-java -cp $CLASSPATH co.kuznetsov.market.Main $SPREADS
+java -Xmx48m -cp $CLASSPATH co.kuznetsov.market.Main $SPREADS
--- bin/market-monitor-macos.sh
+++ bin/market-monitor-macos.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 export SPREADS=~/Dropbox/Covert/Finance/Options/SpreadsMonitoring/spreads.txt
 export CLASSPATH=../lib/*:../sounds:../lib/macos64/*:../out/production/market-monitor
-java -XstartOnFirstThread -cp $CLASSPATH co.kuznetsov.market.Main $SPREADS
+java -Xmx48m -XstartOnFirstThread -cp $CLASSPATH co.kuznetsov.market.Main $SPREADS
