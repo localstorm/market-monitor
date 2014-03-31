4511e3b1b3a4870ae4980373e5a11e606006f7b0
localstorm <alexey@kuznetsov.co>
2014-03-31 10:49:52 +0400

    Win64 changes

--- bin/market-monitor-win64.cmd
+++ bin/market-monitor-win64.cmd
@@ -1,4 +1,4 @@
-SET SPREADS="C:\Documents and Settings\localstorm\Dropbox\Covert\Finance\Options\SpreadsMonitoring\*.txt"
+SET SPREADS=C:\Users\localstorm\Dropbox\Covert\Finance\Options\SpreadsMonitoring\*.txt
 SET CLASSPATH="../lib/*;../lib/win64/*;../sounds;../out/production/market-monitor"
 SET JAVA="C:\Program Files\Java\jdk1.7.0_15\bin\java"
-%JAVA% %1 -Xmx48m -cp %CLASSPATH% co.kuznetsov.market.Main %SPREADS% 
+%JAVA% %1 -Xmx48m -cp %CLASSPATH% co.kuznetsov.market.Main %SPREADS%
